52d958913fc9ab291a0f9eca7c440b707f246d50
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-07-19 18:35:56 -0400

    initial commit

---
+++ Ch03/03_04/finished/my-application/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import video from "./videos/sculpture.mp4";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Art Videos</h1>
+      </header>
+      <div>
+        <video src={video} controls height={200} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
